ce85425e65c291bff0a76ac0208199d5288a88b5
seohasong <tisutoo@korea.ac.kr>
2017-08-03 01:10:46 +0000

    setting tabs

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, Tabs } from 'ionic-angular';
 
+/**
+ * Generated class for the HomePage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  tab1Root='ChatPage';
+  tab2Root='MarketPage';
+  tab3Root='ProfilePage';
 
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad HomePage');
+  }
+
+  @ViewChild('homeTabs') tabRef: Tabs;
+
+  tab_click(tab_number: number){
+    this.tabRef.select(tab_number);
   }
 
 }
