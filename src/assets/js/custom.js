610468035b40e92348db072142b9c1f39b9ea84b
seohasong <tisutoo@korea.ac.kr>
2017-08-03 04:11:33 +0000

    add jquery

---
+++ src/assets/js/custom.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $("#home-footer button").click(function(){
+        $("#home-footer .yellocoin").removeClass("ani-clink");
+        var index = $(this).attr("tab-number");
+        $("#home-footer .yellobar").css("left", index*33+"%");
+        if(index==1){
+            setTimeout(function(){
+                $("#home-footer .yellocoin").addClass("ani-clink");
+            },100)
+        }
+    })
+})
