67147ba497ee9667b15b978cf335a0eb05e6bdbe
toomy0toons <toomy0toons@gmail.com>
2017-08-05 03:33:02 +0900

    gitignore

--- src/app/app.config.ts
+++
@@ -1,8 +0,0 @@
-export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyA5NSgFt0XMJQQ54BnNqyproZAWFJx9VdE",
-    authDomain: "kuber-d2dea.firebaseapp.com",
-    databaseURL: "https://kuber-d2dea.firebaseio.com",
-    projectId: "kuber-d2dea",
-    storageBucket: "kuber-d2dea.appspot.com",
-    messagingSenderId: "493175492388"
-}
